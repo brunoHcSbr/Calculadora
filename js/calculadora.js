b16b2868e47464025cd142958ba45e8171459484
Bruno Henrique <bh089188@gmail.com>
2022-10-21 16:18:16 -0300

    calculadora 1.0

--- js/calculadora.js
+++ js/calculadora.js
@@ -16,41 +16,32 @@ document.getElementById('valor2').addEventListener("change",function(){
 });
 
 function calcular() {
-    
-    if(document.getElementById('valor1').value != '' || document.getElementById('valor2').value != ''){
-        const valor1 = document.getElementById('valor1').value;
-        const valor2 = document.getElementById('valor2').value;
-        const departamento = document.getElementById('departamento').value;
-        const resultado = document.getElementById('resultado').value;
-    
-        if(departamento == "1"){
-            resultado.value = parseInt(valor1) + parseInt(valor2);
-            document.getElementById('resultado').innerText = resultado;
-        }
-        else if(departamento == "2"){
-            resultado.value = valor1 - valor2;
-            
-            document.getElementById('resultado').innerText = resultado;
-        }
-        else if(departamento == "3"){
-            resultado.value = valor1 / valor2;
-            document.getElementById('resultado').innerText = resultado;
-        }
-        else if(departamento == "4"){
-            resultado.value = valor1 * valor2;
-            document.getElementById('resultado').innerText = resultado;
-        }
+    const valor1 = document.getElementById('valor1').value;
+    const valor2 = document.getElementById('valor2').value;
+    const departamento = document.getElementById('departamento').value;
+    const resultado = document.getElementById('resultado');
+if(valor1 == "" || valor2 == ""){
+    alert('Erro!!! Preencha todos os campos !!');
+}
+else{
+    if(departamento == "1"){
+        resultado.value = parseInt(valor1) + parseInt(valor2);
+        document.getElementById('resultado').innerText = resultado;
     }
-    else{
-        if(document.getElementById('valor1') == ''){
-            let valor01=document.getElementById('valor1');
-            verificar(valor01);
-        }
-        else if(document.getElementById('valor2') == ''){
-            let valor02 = document.getElementById('valor2');
-            verificar(valor02);
-        }
+    else if(departamento == "2"){
+        resultado.value = valor1 - valor2;
+
+        document.getElementById('resultado').innerText = resultado;
     }
+    else if(departamento == "3"){
+        resultado.value = valor1 / valor2;
+        document.getElementById('resultado').innerText = resultado;
+    }
+    else if(departamento == "4"){
+        resultado.value = valor1 * valor2;
+        document.getElementById('resultado').innerText = resultado;
+    }
+}
 }
 
 const btnCalcular = document.getElementById('btnCalcular');
